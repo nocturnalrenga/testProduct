d60739a6680594503d85b83e08fee4000cd84082
Renga <repadmanabhan@deloitte.com>
2016-06-12 01:14:16 +0530

    First commit with project template

---
+++ testProduct/testProduct/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testProduct
+//
+//  Created by Padmanabhan, Renganathan (US - Mumbai) on 6/12/16.
+//  Copyright © 2016 deloitte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    //insert dummy comment for Git to pick up
+    
+}
+
